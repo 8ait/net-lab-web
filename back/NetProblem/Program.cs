3b865120125612eaac0bbd36ca811d00f7a44fed
Леонов А.В <leonov22.a.v@gmail.com>
2022-09-18 22:17:45 +0700

    [pr-1] Adding tests.

--- back/NetProblem/Program.cs
+++ back/NetProblem/Program.cs
@@ -1,19 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
+using NetProblem;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+CreateHostBuilder(args).Build().Run();
 
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.Run();
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
